202ec23fed5d6bfa03db1e8979870ded9f83b439
Alexander Barakhov <a.barakhov@payvision.com>
2021-07-01 16:19:19 +0200

    chore: added prettier and eslint formating

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
 module.exports = {
   root: true,
   env: {
+    node: true,
     browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-    'prettier'
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
   ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  globals: {
+    $nuxt: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
 }
