cdab0c436b686f78b0e663786d9926a1f3b361ce
Alexander Barakhov <a.barakhov@payvision.com>
2021-07-02 09:43:08 +0200

    chore: added fonts with google font module

---
+++ components/FontsSample.vue
@@ -0,0 +1,160 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <div class="mr1 ml1 container">
+      <h1>Weather App Concept Prototype 01</h1>
+      <h5>SVG Animation with SCSS & ES6</h5>
+      <p>
+        What looked like a small patch of purple grass, above five feet square,
+        was moving across the sand in their direction. When it came near enough
+        he perceived that it was not grass; there were no blades, but only
+        purple roots. The roots <a href="#">were revolving</a>, for each small
+        plant in the whole patch, like the spokes of a rimless wheel.
+      </p>
+      <p>What looked like a <a href="#">link</a> patch.</p>
+      <p>What looked like a <small>small</small> patch.</p>
+      <p>What looked like a <strong href="#">strong</strong> patch.</p>
+      <p>
+        What looked like a patch of purple grass, above five feet
+        <a href="#">square</a>, was moving across the sand in their direction.
+        When it came near enough he perceived that it was not grass; ther small
+        patch.
+      </p>
+      <h5>A different approach with CSS3</h5>
+      <p>
+        What looked like a small patch of purple grass, above five feet square,
+        was moving across the sand in their direction. When it came near enough
+        he perceived that it was not grass; there were no blades, but only
+        purple roots. The roots were revolving, for each small plant in the
+        whole patch, like the spokes of a rimless wheel.
+      </p>
+      <p>What looked like a small patch.</p>
+      <p>
+        What looked like a patch of purple grass, above five feet square, was
+        moving across the sand in their direction. When it came near enough he
+        perceived that it was not grass; ther small patch.
+      </p>
+      <p class="small_fs">— My name is Manolo, but they call me Lolo.</p>
+
+      <h2>HTML5 SCSS ES6 Jekyll Webpack Boilerplate 02-20</h2>
+
+      <h6>
+        Get the lates version on <a href="www.github.com">Github</a> 06/20
+      </h6>
+
+      <p>
+        What looked like a small patch of purple grass, above five feet square,
+        was moving across the sand in their direction. When it came near enough
+        he perceived that it was not grass; there were no blades, but only
+        purple roots. The roots were revolving, for each small plant in the
+        whole patch, like the spokes of a rimless wheel.
+      </p>
+
+      <h3>CSS3 Grid layout system 29⧸02⧸2020</h3>
+
+      <p>
+        What looked like a small patch of purple grass, above five feet square,
+        was moving across the sand in their direction. When it came near enough
+        he perceived that it was not grass; there were no blades, but only
+        purple roots. The roots were revolving, for each small plant in the
+        whole patch, like the spokes of a rimless wheel.
+      </p>
+
+      <h4>Atomic design methodology based UI components</h4>
+
+      <p>
+        What looked like a small patch of purple grass, above five feet square,
+        was moving across the sand in their direction. When it came near enough
+        he perceived that it was not grass; there were no blades, but only
+        purple roots. The roots were revolving, for each small plant in the
+        whole patch, like the spokes of a rimless wheel.
+      </p>
+      <p class="small_fs">— Sam Smartson</p>
+
+      <h4>Flexbox property based SCSS grid system</h4>
+
+      <p>
+        What looked like a small patch of purple grass, above five feet square,
+        was moving across the sand in their direction. When it came near enough
+        he perceived that it was not grass; there were no blades, but only
+        purple <a href="#">roots</a>. The roots were revolving, for each small
+        plant in the whole patch, like the spokes of a rimless wheel.
+      </p>
+
+      <h4>ultra⧸ugly⧸project</h4>
+
+      <p>
+        What looked like a small patch of purple grass, above five feet square,
+        was moving across the sand in their direction. When it came near enough
+        he perceived that it was not grass; there were no blades, but only
+        purple roots. The roots were revolving, for each small plant in the
+        whole patch, like the spokes of a rimless wheel.
+      </p>
+
+      <h5>Canvas playground</h5>
+
+      <p>
+        What looked like a small patch of purple grass, above five feet square,
+        was moving across the sand in their direction. When it came near enough
+        he perceived that it was not grass; there were no blades, but only
+        purple roots. The roots were revolving, for each small plant in the
+        whole patch, like the spokes of a rimless wheel.
+      </p>
+
+      <h6>GSAP SVG Animations</h6>
+
+      <p>
+        What looked like a small patch of purple grass, above five feet square,
+        was moving across the sand in their direction. When it came near enough
+        he perceived that it was not grass; there were no blades, but only
+        purple roots. The roots were revolving, for each small plant in the
+        whole patch, like the spokes of a rimless wheel.
+      </p>
+      <p>
+        What looked like a small patch of purple grass, above five feet square,
+        was moving across the sand in their direction. When it came near enough
+        he perceived that it was not grass; there were no blades, but only
+        purple roots. The roots were revolving, for each small plant in the
+        whole patch, like the spokes of a rimless wheel. What looked like a
+        small patch of purple grass, above five feet square, was moving across
+        the sand in their direction. When it came near
+        <a href="#">enough he perceived</a> that it was not grass; there were no
+        blades, but only purple roots. The roots were revolving, for each small
+        plant in the whole patch, like the spokes of a rimless wheel. What
+        looked like a small patch of purple grass, above five feet square, was
+        moving across the sand in their direction. When it came near enough he
+        perceived that it was not grass; there were no blades, but only purple
+        roots. The roots were revolving, for each small plant in the whole
+        patch, like the spokes of a rimless wheel.
+      </p>
+      <p>
+        What looked like a small patch of purple grass, above five feet square,
+        was moving across the sand in their direction. When it came near enough
+        he perceived that it was not grass; there were no blades, but only
+        purple roots. The roots were revolving, for each small plant in the
+        whole patch, like the spokes of a rimless wheel.
+      </p>
+
+      <p class="small_fs">— Alexander Barakhov.</p>
+      <h6>
+        A comprehensive approach to SVG path property animation with CSS3 ES6
+        and Figma
+      </h6>
+      <h2>Swiper by iDangero.us</h2>
+      <p>
+        What looked like a small patch of purple grass, above five feet square,
+        was moving across the sand in their direction. When it came near enough
+        he perceived that it was not grass; there were no blades, but only
+        purple roots. The roots were revolving, for each small plant in the
+        whole patch, like the spokes of a rimless wheel.
+      </p>
+
+      <h1>h1 lorem ipsum</h1>
+      <h2>h2 lorem ipsum</h2>
+      <h3>h3 lorem ipsum</h3>
+      <h4>h4 lorem ipsum</h4>
+      <h5>h5 lorem ipsum</h5>
+      <h6>h6 lorem ipsum</h6>
+    </div>
+  </div>
+</template>
